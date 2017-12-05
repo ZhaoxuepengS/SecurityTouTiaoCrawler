0b4361bd5040cfb3f0c0e2fe394075b25941cb4a
Zhaoxuepeng <495149700@qq.com>
2017-12-05 21:07:18 +0800

    update

--- src/cn/uniview/securityCrawler/Crawler.java
+++ src/cn/uniview/securityCrawler/Crawler.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -26,16 +27,18 @@ import CVEs.CVEs;
 
 public class Crawler {
 	public static Logger log = Logger.getLogger("Test");
-	public static StringBuffer sb1 = new StringBuffer("...............开源组件相关漏洞：...................\r\n\r\n");
-	public static StringBuffer sb2 = new StringBuffer("...............开源组件无关漏洞：...................\r\n\r\n");
+	public static StringBuffer sb1 = new StringBuffer("<h1>...............开源组件相关漏洞：...................<h1><br /><br />");
+	public static StringBuffer sb2 = new StringBuffer("<h1>...............开源组件无关漏洞：...................<h1><br /><br />");
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try{
 		log.info("开始抓取头条今日漏洞信息");
 		String content1 = traceToutiao();
 		log.info("开始抓取CVE今日漏洞信息");
 		String content2 = traceCVE();
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("D:\\CVECrawler\\test.txt")));
+		File attament = new File("D:\\Zhaoxuepeng\\test.html");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(attament));
 		bw.write(sb1.toString());
 		bw.write(sb2.toString());
 //		bw.write("头条跟踪完整信息。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。");
@@ -45,9 +48,12 @@ public class Crawler {
 //		bw.write(content2);
 		bw.flush();
 		bw.close();
-		String content = sb1.toString()+"\r\n"+sb2.toString();
+		String content = sb1.toString()+"<br />"+sb2.toString();
 		sendMails mail = new sendMails();
-		mail.sendMail("495149700@qq.com","今日开源组件CVE更新漏洞及头条漏洞",content);
+		mail.sendMail("495149700@qq.com","今日开源组件CVE更新漏洞及头条漏洞",content,attament);
+		}catch(Exception e){
+			log.error(e.getStackTrace());
+		}
 //		String url = "http://cve.mitre.org/cgi-bin/cvename.cgi?name=2017-17096";
 //		Document doc = Jsoup.connect(url).ignoreHttpErrors(true).timeout(100000).get();
 //		Elements s = doc.select("#GeneratedTable > table > tbody > tr:nth-child(4) > td");
@@ -56,11 +62,11 @@ public class Crawler {
 	private static String traceCVE() throws IOException{
 		StringBuffer sb = new StringBuffer();
 		readOSExcel readCom = new readOSExcel();
-		List<Components> list = readCom.readOSComponentExcel("开源组件使用版本跟踪列表.xlsx");
-		sb1.append("CVE相关组件漏洞信息：\r\n");
-		sb1.append("\r\n");
-		sb2.append("CVE无关组件漏洞信息：\r\n");
-		sb2.append("\r\n");
+		List<Components> list = readCom.readOSComponentExcel("D:\\Zhaoxuepeng\\开源组件使用版本跟踪列表.xlsx");
+		sb1.append("<h2>CVE相关组件漏洞信息：<h2><br />");
+		sb1.append("<br />");
+		sb2.append("<h2>CVE无关组件漏洞信息：<h2><br />");
+		sb2.append("<br />");
 		String url = "https://cassandra.cerias.purdue.edu/CVE_changes/today.html";
 		long start = System.currentTimeMillis();  
 		Document doc = Jsoup.connect(url).ignoreHttpErrors(true).timeout(50000).get();
@@ -84,28 +90,28 @@ public class Crawler {
 				Components com = it.next();
 				comName = com.getcomponentName();
 				if(description.contains(comName)){
-					sb1.append("\r\n");
-					sb1.append("漏洞相关组件："+com.getDepartment()+":"+comName);
-					sb1.append("\r\n");
-					sb1.append(cveNo);
-					sb1.append("\r\n");
-					sb1.append(description);
-					sb1.append("\r\n");
+					sb1.append("<br />");
+					sb1.append("<font size=4>漏洞相关组件："+com.getDepartment()+":"+comName+"</font>");
+					sb1.append("<br />");
+					sb1.append("<font size=4>"+cveNo+"</font>");
+					sb1.append("<br />");
+					sb1.append("<font size=4>"+description+"</font>");
+					sb1.append("<br />");
 					iscontain = true;
 				}
 			}
 			if(!iscontain){
-				sb2.append("\r\n");
-				sb2.append(cveNo);
-				sb2.append("\r\n");
-				sb2.append(description);
-				sb2.append("\r\n");
+				sb2.append("<br />");
+				sb2.append("<font size=4>"+cveNo+"</font>");
+				sb2.append("<br />");
+				sb2.append("<font size=4>"+description+"</font>");
+				sb2.append("<br />");
 			}
-			sb.append("\r\n");
+			sb.append("<br />");
 			sb.append(cveNo);
-			sb.append("\r\n");
+			sb.append("<br />");
 			sb.append(description);
-			sb.append("\r\n");
+			sb.append("<br />");
 		}
 		log.info("CVE更新漏洞抓取写入完毕");
 		return sb.toString();
@@ -118,7 +124,7 @@ public class Crawler {
 	
 		Document doc = Jsoup.connect(url).ignoreHttpErrors(true).timeout(10000).get();
 		log.info("连接头条网页");
-		File poFile = new File("D:\\pointer.txt");
+		File poFile = new File("D:\\Zhaoxuepeng\\pointer.txt");
 		
 		BufferedReader bfread = new BufferedReader(new FileReader(poFile));
 		String pointString = bfread.readLine();
@@ -196,10 +202,10 @@ loop:	while(i<11){
 		List<Components> list = readCom.readOSComponentExcel("开源组件使用版本跟踪列表.xlsx");
 		Iterator<articles> it = artList.iterator();
 		StringBuffer contentbf = new StringBuffer();
-		sb1.append("头条相关组件漏洞信息：\r\n");
-		sb1.append("\r\n");
-		sb2.append("头条无关组件漏洞信息：\r\n");
-		sb2.append("\r\n");
+		sb1.append("<h2>头条相关组件漏洞信息：<h2><br />");
+		sb1.append("<br />");
+		sb2.append("<h2>头条无关组件漏洞信息：<h2><br />");
+		sb2.append("<br />");
 		while(it.hasNext()){
 			articles art = it.next();
 			Iterator<Components> comit = list.iterator();
@@ -207,35 +213,35 @@ loop:	while(i<11){
 			while(comit.hasNext()){
 				Components comp = comit.next();
 				if(art.getTitle().contains(comp.getcomponentName())){
-					sb1.append("\r\n");
-					sb1.append(comp.getcomponentName());
-					sb1.append("\r\n");
-					sb1.append(art.getTitle());
-					sb1.append("\r\n");
-					sb1.append(art.gethref());
-					sb1.append("\r\n");
-					sb1.append(art.getarticle());
-					sb1.append("\r\n");
+					sb1.append("<br />");
+					sb1.append("<font size=4>"+comp.getcomponentName()+"</font>");
+					sb1.append("<br />");
+					sb1.append("<font size=4>"+art.getTitle()+"</font>");
+					sb1.append("<br />");
+					sb1.append("<font size=4>"+art.gethref()+"</font>");
+					sb1.append("<br />");
+					sb1.append("<font size=4>"+art.getarticle()+"</font>");
+					sb1.append("<br />");
 					iscontain = true;
 				}
 			}
 			if(!iscontain){
-				sb2.append("\r\n");
-				sb2.append(art.getTitle());
-				sb2.append("\r\n");
-				sb2.append(art.gethref());
-				sb2.append("\r\n");
-				sb2.append(art.getarticle());
-				sb2.append("\r\n");
+				sb2.append("<br />");
+				sb2.append("<font size=4>"+art.getTitle()+"</font>");
+				sb2.append("<br />");
+				sb2.append("<font size=4>"+art.gethref()+"</font>");
+				sb2.append("<br />");
+				sb2.append("<font size=4>"+art.getarticle()+"</font>");
+				sb2.append("<br />");
 			}
 			contentbf.append(art.getTitle());
-			contentbf.append("\r\n");
+			contentbf.append("<br />");
 			contentbf.append(art.gethref());
-			contentbf.append("\r\n");
+			contentbf.append("<br />");
 			contentbf.append(art.getarticle());
-			contentbf.append("\r\n");
-			contentbf.append("\r\n");
-			contentbf.append("\r\n");
+			contentbf.append("<br />");
+			contentbf.append("<br />");
+			contentbf.append("<br />");
 		}
 
 		return contentbf.toString();
--- src/mail/sendMails.java
+++ src/mail/sendMails.java
@@ -1,19 +1,34 @@
 package mail;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.Message.RecipientType;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
+
+import org.apache.log4j.Logger;
+
+import com.sun.mail.util.MailSSLSocketFactory;
 
 public class sendMails {
-
-    public void sendMail(String mailAdr,String subject,String content){
+	public static Logger log = Logger.getLogger("Test");
+    public void sendMail(String mailAdr,String subject,String content,File attachment) throws UnsupportedEncodingException, GeneralSecurityException{
       
         final Properties props = new Properties();
     
@@ -23,7 +38,11 @@ public class sendMails {
         props.put("mail.user", "495149700@qq.com");
 
         props.put("mail.password", "sfletzinvplfbije");
- 
+        // 开启SSL加密，否则会失败
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.socketFactory", sf);
         Authenticator authenticator = new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -47,7 +66,7 @@ public class sendMails {
             InternetAddress to = new InternetAddress(mailAdr);
             message.setRecipient(RecipientType.TO, to);
             
-            String cc[] = { "wangyuqi@uniview.com", "zhaoxuepeng@qq.com" };
+            String cc[] = { "1543913085@qq.com", "zhaoxuepeng@uniview.com" };
             InternetAddress[] ccTo = new InternetAddress[cc.length];  
             for (int i = 0; i < cc.length; i++) {  
                 System.out.println("发送到:" + cc[i]);  
@@ -55,18 +74,39 @@ public class sendMails {
             }  
             message.setRecipients(RecipientType.CC, ccTo);
 
-            
-            //InternetAddress bcc = new InternetAddress("aaaaa@163.com");
-            //message.setRecipient(RecipientType.CC, bcc);
-            
 
+            // 向multipart对象中添加邮件的各个部分内容，包括文本内容和附件
+            Multipart multipart = new MimeMultipart();
+            // 添加邮件正文
+            BodyPart contentPart = new MimeBodyPart();
+            contentPart.setContent(content, "text/html;charset=UTF-8");
+            multipart.addBodyPart(contentPart);
+            
+            // 添加附件的内容
+            if (attachment != null) {
+                BodyPart attachmentBodyPart = new MimeBodyPart();
+                DataSource source = new FileDataSource(attachment);
+                attachmentBodyPart.setDataHandler(new DataHandler(source));
+               
+                //MimeUtility.encodeWord可以避免文件名乱码
+                attachmentBodyPart.setFileName(MimeUtility.encodeWord(attachment.getName()));
+                multipart.addBodyPart(attachmentBodyPart);
+            }
+            
+            // 将multipart对象放到message中
+            message.setContent(multipart);
+            // 保存邮件
+            message.saveChanges();
+            
             message.setSubject(subject);
 
             message.setContent(content, "text/html;charset=UTF-8");
 
-            Transport.send(message);
-        }catch(MessagingException e){
-            e.printStackTrace();
-        }
+            Transport.send(message, message.getAllRecipients());
+        }catch(Exception e){
+        	log.error(e.getStackTrace());
+        }finally {  
+           
+     }  
     }
 }
